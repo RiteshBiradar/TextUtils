2cc77828a51b483c1330b42fd31bd74b78696333
RiteshBiradar <biradarritesh6@gmail.com>
2024-03-02 13:31:44 +0530

    Updates

---
+++ precache-manifest.890d7f4b2c2978b8bac85950effeeee3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30126f0787e0f772e0c487146bcdfaa",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "bb024c10d638bc6de095",
+    "url": "/TextUtils/static/css/main.e5289a75.chunk.css"
+  },
+  {
+    "revision": "76f483385d9c8b278d41",
+    "url": "/TextUtils/static/js/2.544bedc3.chunk.js"
+  },
+  {
+    "revision": "184544feb51a777f05eb9d4b59832005",
+    "url": "/TextUtils/static/js/2.544bedc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3fea0913eb03f5b184e",
+    "url": "/TextUtils/static/js/3.c7aa61ac.chunk.js"
+  },
+  {
+    "revision": "bb024c10d638bc6de095",
+    "url": "/TextUtils/static/js/main.68d09b08.chunk.js"
+  },
+  {
+    "revision": "0da67f7a6e79c80d7a25",
+    "url": "/TextUtils/static/js/runtime-main.c32cff0e.js"
+  }
+]);
